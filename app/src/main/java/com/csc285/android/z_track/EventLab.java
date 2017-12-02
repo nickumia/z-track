321b51d5de4d062a19a5736e9c3bda61663b7b76
nickumia <nkumia1@pride.hofstra.edu>
2017-12-02 01:38:44 -0500

    Started to implement database functionality.. Created static "event" to hold current event because the event was being recreated on screen rotations..

--- app/src/main/java/com/csc285/android/z_track/EventLab.java
+++ app/src/main/java/com/csc285/android/z_track/EventLab.java
@@ -1,9 +1,16 @@
 package com.csc285.android.z_track;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.csc285.android.z_track.Statistics.Distance;
+import com.csc285.android.z_track.Statistics.Elevation;
+import com.csc285.android.z_track.Statistics.LocationA;
+import com.csc285.android.z_track.Statistics.Pace;
+import com.csc285.android.z_track.Statistics.Time;
+import com.csc285.android.z_track.Statistics.Velocity;
 import com.csc285.android.z_track.database.EventBaseHelper;
 import com.csc285.android.z_track.database.EventCursorWrapper;
 import com.csc285.android.z_track.database.EventDbSchema;
@@ -18,6 +25,7 @@ import java.util.UUID;
 
 public class EventLab {
     private static EventLab sStatisticsLab;
+    private Event tempEvent;
 
     private Context mContext;
     private SQLiteDatabase mDatabase;
@@ -40,7 +48,60 @@ public class EventLab {
 
         mContext = context.getApplicationContext();
         mDatabase = new EventBaseHelper(mContext).getWritableDatabase();
+    }
 
+    private static ContentValues getContentValues(Event event) {
+        ContentValues values = new ContentValues();
+        values.put(EventDbSchema.EventTable.Cols.UUID,
+                event.getmId().toString());
+        values.put(EventDbSchema.EventTable.Cols.DATE,
+                event.getmDate().getTime());
+
+        Distance d = (Distance) event.getStat(R.string.activity_item_distance);
+        values.put(EventDbSchema.EventTable.Cols.DISTANCE,
+                d.getTotalDistance());
+
+        Pace p = (Pace) event.getStat(R.string.activity_item_pace);
+        values.put(EventDbSchema.EventTable.Cols.PACE,
+                p.getPace());
+
+        Elevation e = (Elevation) event.getStat(R.string.activity_item_elevation);
+        values.put(EventDbSchema.EventTable.Cols.ELEVATION,
+                e.getElevation());
+
+        Velocity v = (Velocity) event.getStat(R.string.activity_item_topspeed);
+        values.put(EventDbSchema.EventTable.Cols.TOP_VELOCITY,
+                v.getTopVelocity());
+        values.put(EventDbSchema.EventTable.Cols.VELOCITY,
+                v.getTopVelocity());
+
+        LocationA l = (LocationA) event.getStat(R.string.activity_item_location);
+        values.put(EventDbSchema.EventTable.Cols.START_LOC_LAT,
+                l.getStart().getLatitude());
+        values.put(EventDbSchema.EventTable.Cols.START_LOC_LON,
+                l.getStart().getLongitude());
+        values.put(EventDbSchema.EventTable.Cols.END_LOC_LAT,
+                l.getEnd().getLatitude());
+        values.put(EventDbSchema.EventTable.Cols.END_LOC_LON,
+                l.getEnd().getLongitude());
+        values.put(EventDbSchema.EventTable.Cols.MARKER_LOC,
+                l.getMarkers().get(0).getLongitude());
+
+        Time t = (Time) event.getStat(R.string.activity_item_time);
+        values.put(EventDbSchema.EventTable.Cols.START_TIME,
+                t.getStartTime());
+        values.put(EventDbSchema.EventTable.Cols.END_TIME,
+                t.getEndTime());
+
+        return values;
+    }
+
+    public void updateEvent(Event event) {
+        String uuidString = event.getmId().toString();
+        ContentValues values = getContentValues(event);
+        mDatabase.update(EventDbSchema.EventTable.NAME, values,
+                EventDbSchema.EventTable.Cols.UUID + " = ?",
+                new String[] { uuidString });
     }
 
     public List<Event> getEvents() {
@@ -86,5 +147,13 @@ public class EventLab {
                 null  // orderBy
         );
         return new EventCursorWrapper(cursor);
+    }
+
+    public Event getTempEvent() {
+        return tempEvent;
+    }
+
+    public void setTempEvent(Event tempEvent) {
+        this.tempEvent = tempEvent;
     }
 }
