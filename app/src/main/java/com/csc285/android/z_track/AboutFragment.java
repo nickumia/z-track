8a7334d0dc6892bf1f4499f59db0cbdf15a6a1d4
nickumia <nkumia1@pride.hofstra.edu>
2017-12-06 14:22:06 -0500

    Updated About page..

--- app/src/main/java/com/csc285/android/z_track/AboutFragment.java
+++ app/src/main/java/com/csc285/android/z_track/AboutFragment.java
@@ -1,10 +1,12 @@
 package com.csc285.android.z_track;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by nick on 11/16/2017.
@@ -13,7 +15,9 @@ import android.view.ViewGroup;
 
 public class AboutFragment extends Fragment {
 
-    private static final String TAG = "ActivityFragment";
+    private static final String TAG = "AboutFragment";
+    private TextView mAppTextView;
+    private TextView mBuildTextView;
 
     public static AboutFragment newInstance()
     {
@@ -27,8 +31,12 @@ public class AboutFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_history, container, false);
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
 
+        mAppTextView = (TextView) v.findViewById(R.id.appName);
+        mBuildTextView = (TextView) v.findViewById(R.id.buildTextView);
+
+        mBuildTextView.setText("" + getString(R.string.app_build) + " "+ Build.VERSION.SDK_INT);
         return v;
     }
 }
