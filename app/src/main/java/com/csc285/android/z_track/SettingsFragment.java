e21a19a9a3a4d44c19024139cfd776573663f124
nickumia <nkumia1@pride.hofstra.edu>
2017-12-08 02:45:38 -0500

    Basic settings fragment created.. no functionality yet..

--- app/src/main/java/com/csc285/android/z_track/SettingsFragment.java
+++ app/src/main/java/com/csc285/android/z_track/SettingsFragment.java
@@ -27,7 +27,7 @@ public class SettingsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_history, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
 
         return v;
     }
