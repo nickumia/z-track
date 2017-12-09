9a9d45f78f7370f505bd1ba5de7d902071d857fa
nickumia <nkumia1@pride.hofstra.edu>
2017-12-08 19:28:31 -0500

    Added method to calculate distance between two locations..

--- app/src/main/java/com/csc285/android/z_track/Statistics/Distance.java
+++ app/src/main/java/com/csc285/android/z_track/Statistics/Distance.java
@@ -1,22 +1,60 @@
 package com.csc285.android.z_track.Statistics;
 
+import android.location.Location;
+
+import com.csc285.android.z_track.MainActivity;
+
 /**
- * Created by nicku on 11/17/2017.
+ * Class to track total distance traveled
  */
 
 public class Distance extends Statistics {
 
-    private float totalDistance = 0;
+    private float totalDistanceEN = 0;
+    private float totalDistanceSI = 0;
 
     public float getTotalDistance() {
-        return totalDistance;
+        if (MainActivity.UNIT.equals("SI")) {
+            return totalDistanceSI;
+        } else {
+            return totalDistanceEN;
+        }
     }
 
     public void setTotalDistance(float totalDistance) {
-        this.totalDistance = totalDistance;
+        if (MainActivity.UNIT.equals("SI")) {
+            this.totalDistanceSI = totalDistance;
+        } else {
+            this.totalDistanceEN = totalDistance;
+        }
     }
 
     public void updateDistance(float part){
-        this.totalDistance += part;
+        if (MainActivity.UNIT.equals("SI")) {
+            this.totalDistanceSI = part;
+        } else {
+            this.totalDistanceEN = part;
+        }
+    }
+
+    // Algorithm to calculate distance between two GeoPoints
+    // Adapted from :
+    // https://www.movable-type.co.uk/scripts/latlong.html
+    public double  getDistanceFromLatLonInKm(Location a, Location b) {
+        double R = 6371; // Radius of the earth in km
+        double dLat = deg2rad(b.getLatitude()-a.getLatitude());  // deg2rad below
+        double dLon = deg2rad(b.getLongitude()-a.getLongitude());
+        double aa = Math.sin(dLat/2) *
+                Math.sin(dLat/2) +
+                Math.cos(deg2rad(a.getLongitude())) *
+                Math.cos(deg2rad(b.getLatitude())) *
+                Math.sin(dLon/2) *
+                Math.sin(dLon/2);
+        double c = 2 * Math.atan2(Math.sqrt(aa), Math.sqrt(1-aa));
+        return R * c; // Distance in km
+    }
+
+    double deg2rad(double deg) {
+        return deg * (Math.PI/180);
     }
 }
