379b41a7ff7018e330dc0e6b0ba4d5b007157c44
nickumia <nkumia1@pride.hofstra.edu>
2017-12-08 19:31:02 -0500

    Added locatiton data to large picture dialog..

--- app/src/main/java/com/csc285/android/z_track/ShowLargePictureFragment.java
+++ app/src/main/java/com/csc285/android/z_track/ShowLargePictureFragment.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 
@@ -22,24 +23,28 @@ import java.io.File;
 public class ShowLargePictureFragment extends DialogFragment {
 
     private static final String ARG_PIC = "large_picture";
-    public static final String EXTRA_PIC =
-            "com.csc285.android.z_track.large_picture";
+    private static final String ARG_POS = "geo_position";
+    public static final String EXTRA_PIC = "com.csc285.android.z_track.large_picture";
+    public static final String EXTRA_POS = "com.csc285.android.z_track.geo_position";
     private ImageView mLargePicture;
+    private TextView mPositionView;
     private File mPhotoFile;
 
     @Override @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         String pic_Crime = (String) getArguments().getSerializable(ARG_PIC);
+        String position = (String) getArguments().getString(ARG_POS);
 
         View v = LayoutInflater.from(getActivity())
                 .inflate(R.layout.dialog_show_pictore, null);
 
         mLargePicture = (ImageView) v.findViewById(R.id.marker_photo_large);
-//        mPhotoFile = CrimeLab.get(getActivity()).getPhotoFile(pic_Crime);
+        mPositionView = (TextView) v.findViewById(R.id.positionView);
         mPhotoFile = new File(getActivity().getApplicationContext().getFilesDir(), pic_Crime);
         Bitmap bitmap = PictureUtils.getScaledBitmap(mPhotoFile.getPath(), getActivity());
         mLargePicture.setImageBitmap(bitmap);
+        mPositionView.setText(position);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
@@ -53,9 +58,10 @@ public class ShowLargePictureFragment extends DialogFragment {
                 .create();
     }
 
-    public static ShowLargePictureFragment newInstance(String pCrime) {
+    public static ShowLargePictureFragment newInstance(String pCrime, String position) {
         Bundle args = new Bundle();
         args.putSerializable(ARG_PIC, pCrime);
+        args.putString(ARG_POS, position);
         ShowLargePictureFragment fragment = new ShowLargePictureFragment();
         fragment.setArguments(args);
         return fragment;
