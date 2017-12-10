50a8c86abe043e63ac20df1630c75dd1068baa5d
nickumia <nkumia1@pride.hofstra.edu>
2017-12-10 02:19:03 -0500

    Tentatively final DB design..

--- app/src/main/java/com/csc285/android/z_track/database/EventDbSchema.java
+++ app/src/main/java/com/csc285/android/z_track/database/EventDbSchema.java
@@ -16,14 +16,14 @@ public class EventDbSchema {
 //            public static final String TIME = "time";
             public static final String DISTANCE = "distance";
             public static final String PACE = "pace";
-            public static final String ELEVATION = "elevation";
+//            public static final String ELEVATION = "elevation";
             public static final String TOP_VELOCITY = "velocity_top";
-            public static final String VELOCITY = "velocity";
+            public static final String AVG_VELOCITY = "velocity_avg";
             public static final String START_LOC_LAT = "location_start_lat";
             public static final String START_LOC_LON = "location_start_lon";
             public static final String END_LOC_LAT = "location_end_lat";
             public static final String END_LOC_LON = "location_end_lon";
-            public static final String MARKER_LOC = "location_markers";
+//            public static final String MARKER_LOC = "location_markers";
             public static final String TIME_M = "time_minutes";
             public static final String TIME_S = "time_seconds";
             public static final String TIME_MS = "time_milli";
@@ -38,7 +38,7 @@ public class EventDbSchema {
             public static final String UUID = "uuid";
             public static final String NUM = "num";
             public static final String VEL = "vel";
-            public static final String PACE = "pace";
+//            public static final String PACE = "pace";
         }
     }
 
@@ -60,6 +60,7 @@ public class EventDbSchema {
             public static final String LATITUDE = "latitude";
             public static final String LONGITUDE = "longitude";
             public static final String PHOTO = "photo_name";
+            public static final String INFO = "info";
         }
     }
 
