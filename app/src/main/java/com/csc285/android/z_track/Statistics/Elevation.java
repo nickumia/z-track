d5599634c9efbce17b74cf59255f8729a31b9df9
nickumia <nkumia1@pride.hofstra.edu>
2017-12-10 02:16:56 -0500

    Modified setElevation to get correct index from db..

--- app/src/main/java/com/csc285/android/z_track/Statistics/Elevation.java
+++ app/src/main/java/com/csc285/android/z_track/Statistics/Elevation.java
@@ -1,20 +1,22 @@
 package com.csc285.android.z_track.Statistics;
 
+import java.util.ArrayList;
+
 /**
  * Created by nicku on 11/17/2017.
  */
 
 public class Elevation extends Statistics {
 
-    private float elevation = 0;
+    private ArrayList<Float> elevation = new ArrayList<>();
     private float elevationRate = 0;
 
-    public float getElevation() {
+    public ArrayList<Float> getElevation() {
         return elevation;
     }
 
-    public void setElevation(float elevation) {
-        this.elevation = elevation;
+    public void setElevation(float elevation, int i) {
+        this.elevation.add(i, elevation);
     }
 
     public float getElevationRate() {
