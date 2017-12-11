e7f23976517edc8738e97d9750f7b875bac7792f
nickumia <nkumia1@pride.hofstra.edu>
2017-12-11 16:08:16 -0500

    New statistics..

---
+++ app/src/main/java/com/csc285/android/z_track/Statistics/Rating.java
@@ -0,0 +1,18 @@
+package com.csc285.android.z_track.Statistics;
+
+/**
+ * Created by nicku on 11/17/2017.
+ */
+
+public class Rating extends Statistics{
+
+    private int rating = 0;
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
